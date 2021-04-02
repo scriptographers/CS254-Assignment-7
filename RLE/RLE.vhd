d0eb8e97690f95e0a317323c25b8911fb9608855
devansh-dvj <devansh.dvj@gmail.com>
2021-04-02 22:20:36 +0530

    RLE: Output works

--- RLE/RLE.vhd
+++ RLE/RLE.vhd
@@ -17,44 +17,62 @@ entity RLE is
 end entity;
 
 architecture struct of RLE is
-  type offsets_type is array (20 downto 0) of std_logic_vector (7 downto 0);
+  type in_offset is array (50 downto 0) of std_logic_vector (7 downto 0);
+  type out_offset is array (100 downto 0) of std_logic_vector (7 downto 0);
   signal previous : std_logic_vector (7 downto 0);
   signal previous1 : std_logic_vector (7 downto 0);
   signal count : unsigned (7 downto 0) := "00000000";
   signal input_counter : integer := 0;
   signal output_counter : integer := 0;
-  signal proc_counter : integer := 0;
+  signal proc_counter : integer := 1;
   signal char_counter : integer := 1;
-  signal signal_counter : integer := 0;
-  signal in_buf : offsets_type;
-  signal out_buf : offsets_type;
+  signal out_counter : integer := 0;
+  signal in_buf : in_offset;
+  signal out_buf : out_offset;
 begin
 
   process (clk)
   begin
     if (rising_edge(clk)) then
-      if (count < "00001001") then
+      -- if (count < "1000001") then
         input_counter <= input_counter + 1;
         in_buf(input_counter) <= a;
         -- ints <= input_counter;
 
         if (input_counter > 1) then
           proc_counter <= proc_counter + 1;
-        end if;
-        if (in_buf(proc_counter) = in_buf(proc_counter + 1)) then
-          char_counter <= char_counter + 1;
-        else
-          char_counter <= 1;
+          if (in_buf(proc_counter) = in_buf(proc_counter - 1)) then
+            char_counter <= char_counter + 1;
+          else
+            if (char_counter > 2) then
+              out_buf(out_counter) <= "00011011";
+              out_buf(out_counter + 1) <= std_logic_vector(to_unsigned(char_counter, 8));
+              out_buf(out_counter + 2) <= in_buf(proc_counter - 1);
+              out_counter <= out_counter + 3;
+            elsif (char_counter = 2) then
+              out_buf(out_counter) <= in_buf(proc_counter - 1);
+              out_buf(out_counter + 1) <= in_buf(proc_counter - 1);
+              out_counter <= out_counter + 2;
+            else
+              out_buf(out_counter) <= in_buf(proc_counter - 1);
+              out_counter <= out_counter + 1;
+            end if;
+            char_counter <= 1;
+          end if;
         end if;
 
         ints <= char_counter;
 
-        dataline <= '1';
-        if (count = "00001000") then
+        if (output_counter < out_counter) then
+          z <= out_buf(output_counter);
+          dataline <= '1';
+          output_counter <= output_counter + 1;
+        else
           dataline <= '0';
         end if;
-        count <= count + 1;
-      end if;
+
+      --   count <= count + 1;
+      -- end if;
     end if;
   end process;
 
